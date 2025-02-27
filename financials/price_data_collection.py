20e2f74f4a7c505d430b1c17f3188186286010a3
Vindhyaa Saravanan <vindhyaa.saravanan@yahoo.co.in>
2025-02-27 16:53:37 +0000

    Added prices from 1 Jan 2020

--- financials/price_data_collection.py
+++ financials/price_data_collection.py
@@ -1,12 +1,17 @@
 import yfinance as yf
 import pandas as pd
 import os
+from datetime import datetime, timedelta
 
 # Define the crude oil ticker symbols
 symbols = ['CL=F', 'BZ=F']
 
-# Fetch historical data (1 year of daily data)
-data = yf.download(symbols, period="1y", interval="1d")
+# Define the start and end dates
+start_date = "2020-01-01"
+end_date = (datetime.today() + timedelta(days=1)).strftime('%Y-%m-%d')  # Tomorrow's date
+
+# Fetch historical data
+data = yf.download(symbols, start=start_date, end=end_date, interval="1d")
 
 # Select only the required columns
 filtered_data = data[['Open', 'Close']]
@@ -17,7 +22,7 @@ filtered_data = filtered_data.reset_index()
 filtered_data['Date'] = filtered_data['Date'].dt.strftime('%d-%m-%Y')
 
 # Define the path to save the file
-file_path = os.path.expanduser('~/Downloads/filtered_crude_oil_prices.csv')
+file_path = os.path.expanduser('wti_brent_crude_oil_prices.csv')
 
 # Display first few rows
 print(filtered_data.head())
--- financials/price_data_upload.py
+++ financials/price_data_upload.py
@@ -9,11 +9,11 @@ uri = "mongodb+srv://geopulse5530:x1GJ55GaO0p87U2I@geopulse.oniyq.mongodb.net/?r
 client = MongoClient(uri, server_api=ServerApi('1'))
 
 # Select database and collection
-db = client["Prices-1year"]  # Change to your desired database name
-collection = db["Finances"]  # Change to your desired collection name
+db = client["Prices-1year"] 
+collection = db["Finances"] 
 
 # Load CSV data
-csv_file_path = "~/Downloads/filtered_crude_oil_prices.csv"  # Update the path
+csv_file_path = "wti_brent_crude_oil_prices.csv"  
 df = pd.read_csv(csv_file_path)
 
 # Convert DataFrame to dictionary
